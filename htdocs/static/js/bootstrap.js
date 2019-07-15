0b01e471b676bd7452f8f0cf2e3c9a489aa5580e
loeye <loeye@ZhangYi-PC>
2019-07-15 14:39:49 +0800

    move app sample

--- htdocs/static/js/bootstrap.js
+++
@@ -1,6 +0,0 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License"),
- * see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-import '/bootstrap/dist/js/bootstrap.min.js'
