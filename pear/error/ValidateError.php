065b4cc806ca832675e354ef6382d35eb9630cca
loeye <loeye@ZhangYi-PC>
2019-08-04 16:16:27 +0800

    增加验证错误异常类

---
+++ pear/error/ValidateError.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * ValidateError.php
+ *
+ * PHP version 7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * @category PHP
+ * @package  LOEYE
+ * @author   Zhang Yi <loeyae@gmail.com>
+ * @version  2019-8-4 16:07:55
+ * @link     https://github.com/loeyae/loeye.git
+ */
+
+namespace loeye\error;
+
+/**
+ * ValidateError
+ *
+ * @author   Zhang Yi <loeyae@gmail.com>
+ */
+class ValidateError extends \loeye\error\BusinessException
+{
+
+    const DEFAULT_ERROR_MSG = "validate has error";
+
+    const DEFAULT_ERROR_CODE = 510000;
+
+    /**
+
+     * @var array
+     */
+    protected $validateMessage;
+
+    public function __construct(array $validateMessage, string $errorMessage = self::DEFAULT_ERROR_MSG, int $errorCode = self::DEFAULT_ERROR_CODE, $parameter = array()): void
+    {
+        parent::__construct($errorMessage, $errorCode, $parameter);
+        $this->validateMessage = $validateMessage;
+    }
+
+    /**
+     * getValidateMessage
+     *
+     * @return array|null
+     */
+    public function getValidateMessage()
+    {
+        return $this->validateMessage;
+    }
+}
