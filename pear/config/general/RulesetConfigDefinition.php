f86bd8b61d0ad076cbb40260bcfc9bf21fa3c376
ZhangYi <loeyae@gmail.com>
2020-01-16 23:20:32 +0800

    增加通用配置文件规则

---
+++ pear/config/general/RulesetConfigDefinition.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * RulesetConfigDefinition.php
+ *
+ * PHP version 7
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * @category PHP
+ * @package  LOEYE
+ * @author   Zhang Yi <loeyae@gmail.com>
+ * @version  2020年1月8日 下午9:56:03
+ * @link     https://github.com/loeyae/loeye.git
+ */
+
+namespace loeye\config\general;
+
+use \loeye\config\TreeBuilder;
+
+/**
+ * RulesetConfigDefinition
+ *
+ * @author   Zhang Yi <loeyae@gmail.com>
+ */
+class RulesetConfigDefinition implements \Symfony\Component\Config\Definition\ConfigurationInterface {
+
+
+    /**
+     * getConfigTreeBuilder
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('-');
+        $treeBuilder->getRootNode()
+                 ->children()
+                     ->arrayNode('settings')
+                         ->children()
+                             ->scalarNode('0')->isRequired()->defaultValue('master')->end()
+                         ->end()
+                     ->end()
+                     ->regexNode('*')
+                        ->variablePrototype()->end()
+                     ->end()
+                 ->end();
+        return $treeBuilder;
+    }
+
+
+}
