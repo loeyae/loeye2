c372761172c92691c6c89c6a182543853c8adeab
ZhangYi <loeyae@gmail.com>
2020-02-20 19:50:50 +0800

    优化配置文件生成器

--- pear/commands/GenerateConfig.php
+++ pear/commands/GenerateConfig.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * CreateConfig.php
+ * GenerateConfig.php
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"),
  * see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
@@ -19,13 +19,13 @@ use \Symfony\Component\Console\{
 };
 
 /**
- * CreateConfig
+ * GenerateConfig
  *
  * @author   Zhang Yi <loeyae@gmail.com>
  */
-class CreateConfig extends Command {
+class GenerateConfig extends Command {
 
-    protected $name   = 'loeye:create-config';
+    protected $name   = 'loeye:generate-config';
     protected $args   = [
         ['property', 'required' => true, 'help' => 'property name', 'default' => null],
         ['type', 'required' => true, 'help' => 'configuration type: app-master,app-delta,cache,database,module,router,valid-rule,valid-delta,ruleset,delta', 'default' => null],
@@ -33,19 +33,19 @@ class CreateConfig extends Command {
     protected $params = [
         ['file', 'f', 'required' => false, 'help' => 'file name', 'default' => 'master']
     ];
-
     static protected $configDefinition = [
-            'app-master'  => \loeye\config\app\ConfigDefinition::class,
-            'app-delta'   => \loeye\config\app\DeltaDefinition::class,
-            'cache'       => \loeye\config\cache\ConfigDefinition::class,
-            'database'    => \loeye\config\database\ConfigDefinition::class,
-            'module'      => \loeye\config\module\ConfigDefinition::class,
-            'router'      => \loeye\config\router\ConfigDefinition::class,
-            'valid-rule'  => \loeye\config\validate\RulesetConfigDefinition::class,
-            'valid-delta' => \loeye\config\validate\DeltaConfigDefinition::class,
-            'ruleset'     => \loeye\config\general\RulesetConfigDefinition::class,
-            'delta'       => \loeye\config\general\DeltaConfigDefinition::class,
-        ];
+        'app-master'  => \loeye\config\app\ConfigDefinition::class,
+        'app-delta'   => \loeye\config\app\DeltaDefinition::class,
+        'cache'       => \loeye\config\cache\ConfigDefinition::class,
+        'database'    => \loeye\config\database\ConfigDefinition::class,
+        'module'      => \loeye\config\module\ConfigDefinition::class,
+        'router'      => \loeye\config\router\ConfigDefinition::class,
+        'valid-rule'  => \loeye\config\validate\RulesetConfigDefinition::class,
+        'valid-delta' => \loeye\config\validate\DeltaConfigDefinition::class,
+        'ruleset'     => \loeye\config\general\RulesetConfigDefinition::class,
+        'delta'       => \loeye\config\general\DeltaConfigDefinition::class,
+    ];
+
 
     /**
      * process
@@ -57,17 +57,18 @@ class CreateConfig extends Command {
      */
     public function process(InputInterface $input, OutputInterface $output)
     {
-        $property = $input->getArgument('property');
-        $type = $input->getArgument('type');
-        $name = $input->getOption('file');
-        $file = $this->getConfigPath($property, $type, $name);
-        $dumper = new \Symfony\Component\Config\Definition\Dumper\YamlReferenceDumper();
+        $property      = $input->getArgument('property');
+        $type          = $input->getArgument('type');
+        $name          = $input->getOption('file');
+        $file          = $this->getConfigPath($property, $type, $name);
+        $dumper        = new \Symfony\Component\Config\Definition\Dumper\YamlReferenceDumper();
         $configuration = $this->getConfiguration($type);
-        $content = $dumper->dump($configuration);
-        $fileSystem = new \Symfony\Component\Filesystem\Filesystem();
+        $content       = $dumper->dump($configuration);
+        $fileSystem    = new \Symfony\Component\Filesystem\Filesystem();
         $fileSystem->dumpFile($file, $content);
         $output->writeln(sprintf("output file: %1s", $file));
     }
+
 
     /**
      * getConfiguration
@@ -84,7 +85,8 @@ class CreateConfig extends Command {
         }
         return null;
     }
-    
+
+
     /**
      * getConfigPath
      * 
@@ -98,18 +100,19 @@ class CreateConfig extends Command {
         switch ($type) {
             case 'app-master':
             case 'app-delta':
-                return PROJECT_CONFIG_DIR .D_S. $property .D_S. 'app' .D_S. $file .'.yml';
+                return PROJECT_CONFIG_DIR . D_S . $property . D_S . 'app' . D_S . $file . '.yml';
             case 'cache':
             case 'database':
-                return PROJECT_CONFIG_DIR .D_S. $property .D_S. $type .D_S. $file .'.yml';
+                return PROJECT_CONFIG_DIR . D_S . $property . D_S . $type . D_S . $file . '.yml';
             case 'module':
+                return PROJECT_CONFIG_DIR . D_S . 'modules' . D_S . $property . D_S . $file . '.yml';
             case 'router':
-                return PROJECT_CONFIG_DIR .D_S. $type .D_S. $property .D_S. $file .'.yml';
+                return PROJECT_CONFIG_DIR . D_S . $type . D_S . $property . D_S . $file . '.yml';
             case 'valid-rule':
             case 'valid-delta':
-                return PROJECT_CONFIG_DIR .D_S. $property .D_S. 'validate' .D_S. $file .'.yml';
+                return PROJECT_CONFIG_DIR . D_S . 'validate' . D_S . $property . D_S . $file . '.yml';
             default:
-                return PROJECT_CONFIG_DIR .D_S. $property .D_S. 'general' .D_S. $file .'.yml';
+                return PROJECT_CONFIG_DIR . D_S . $property . D_S . 'general' . D_S . $file . '.yml';
                 break;
         }
     }
