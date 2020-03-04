da4dd315dde54931c78a6ebcb6cfa7372fd994f6
loeye <loeye@ZhangYi-PC>
2020-03-04 18:14:13 +0800

    新增验证方式

--- pear/plugin/ValidatePlugin.php
+++ pear/plugin/ValidatePlugin.php
@@ -17,40 +17,82 @@
 
 namespace loeye\plugin;
 
-use loeye\base\{
-    Context,
-    Utils,
-    Validator
-};
+use loeye\base\Context;
+use loeye\base\Utils;
+use loeye\base\Validator;
+use loeye\std\Plugin;
 
 /**
  * Description of ValidatePlugin
  *
  * @author   Zhang Yi <loeyae@gmail.com>
  */
-class ValidatePlugin extends \loeye\std\Plugin
+class ValidatePlugin extends Plugin
 {
+
+    const INPUT_ORIGIN   = 2;
+    const ENTITY_KEY     = 'entity';
+    const RULE_KEY       = 'validate_rule';
+    const BUNDLE_KEY     = 'bundle';
+    const INPUT_TYPE_KEY = 'type';
+    const ERROR_KEY      = __CLASS__ . '_validate_error';
+    const DATA_KEY       = __CLASS__ . '_filter_data';
+
+    static public $inputTypes = [
+        \INPUT_REQUEST,
+        self::INPUT_ORIGIN,
+        \INPUT_POST,
+        \INPUT_GET,
+    ];
 
     /**
      * process
      *
-     * @param \loeye\base\Context $context context
+     * @param Context $context context
      * @param array               $inputs  inputs
      *
      * @return void
      */
-    public function process(\loeye\base\Context $context, array $inputs)
+    public function process(Context $context, array $inputs)
     {
-        $rule       = \loeye\base\Utils::checkNotEmpty($inputs, 'validate_rule');
-        $custBundle = \loeye\base\Utils::getData($inputs, 'bundle', null);
-        $validation = new Validator($context->getAppConfig(), $custBundle);
-        $report     = $validation->validate($_REQUEST, $rule);
-        if ($report['has_error'] == true) {
-            \loeye\base\Utils::addErrors(
-                    $report['error_message'], $context, $inputs, __CLASS__ . '_validate_error');
+        $type = Utils::getData($inputs, self::INPUT_TYPE_KEY, \INPUT_REQUEST);
+        switch ($type) {
+            case INPUT_POST:
+                $data = filter_input_array(INPUT_POST);
+                break;
+            case INPUT_GET:
+                $data = filter_input_array(INPUT_GET);
+                break;
+            case self::INPUT_ORIGIN:
+                $data = file_get_contents("php://");
+                $data = \json_decode($data, true);
+                break;
+            default:
+                $data = filter_input_array(INPUT_REQUEST);
+                break;
         }
-        \loeye\base\Utils::setContextData(
-                $report['valid_data'], $context, $inputs, __CLASS__ . '_filter_data');
+        $entity = Utils::getData($inputs, self::ENTITY_KEY);
+        if ($entity) {
+            $entityObject = Utils::source2entity($data, $entity);
+            $validator     = \Symfony\Component\Validator\Validation::createValidator();
+            $violationList = $validator->validate($entityObject);
+            $errors        = Validator::buildErrmsg($violationList);
+            if ($errors) {
+                Utils::addErrors($errors, $context, $inputs, self::ERROR_KEY);
+            }
+            Utils::setContextData($entityObject, $context, $inputs, self::DATA_KEY);
+        } else {
+            $rule       = Utils::checkNotEmpty($inputs, self::RULE_KEY);
+            $custBundle = Utils::getData($inputs, self::BUNDLE_KEY, null);
+            $validation = new Validator($context->getAppConfig(), $custBundle);
+            $report     = $validation->validate($data, $rule);
+            if ($report['has_error'] == true) {
+                Utils::addErrors(
+                        $report['error_message'], $context, $inputs, self::ERROR_KEY);
+            }
+            Utils::setContextData(
+                    $report['valid_data'], $context, $inputs, self::DATA_KEY);
+        }
     }
 
 }
