af87822920136766b3674e8c133b9cb204db0d72
ZhangYi <loeyae@gmail.com>
2020-04-18 08:49:16 +0800

    增加代码覆盖率输出配置

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,20 +12,21 @@ require_once(dirname(__DIR__) .DIRECTORY_SEPARATOR .'vendor'. DIRECTORY_SEPARATO
 define('PROJECT_UNIT_DIR', __DIR__);
 define('PROJECT_UNIT_RUNTIME_DIR', PROJECT_UNIT_DIR.DIRECTORY_SEPARATOR.'runtime');
 
-//dirname(PROJECT_UNIT_DIR) .'/output/code-coverage-report');use SebastianBergmann\CodeCoverage\CodeCoverage;
-////use SebastianBergmann\CodeCoverage\Report\Clover;
-////use SebastianBergmann\CodeCoverage\Report\Html\Facade;
-////
-////$coverage = new CodeCoverage();
-////
-////$coverage->filter()->addDirectoryToWhitelist(dirname(PROJECT_UNIT_DIR) .'/pear');
-////
-////$coverage->start('<name of test>');
-////
-////$coverage->stop();
-////
-////$writer = new Clover;
-////$writer->process($coverage, dirname(PROJECT_UNIT_DIR) .'/output/coverage/clover.xml');
-////
-////$writer = new Facade;
-////$writer->process($coverage,
+
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Report\Clover;
+use SebastianBergmann\CodeCoverage\Report\Html\Facade;
+
+$coverage = new CodeCoverage();
+
+$coverage->filter()->addDirectoryToWhitelist(dirname(PROJECT_UNIT_DIR) .'/pear');
+
+$coverage->start('<name of test>');
+
+$coverage->stop();
+
+$writer = new Clover;
+$writer->process($coverage, dirname(PROJECT_UNIT_DIR) .'/output/coverage/clover.xml');
+
+$writer = new Facade;
+$writer->process($coverage,dirname(PROJECT_UNIT_DIR) .'/output/code-coverage-report');
